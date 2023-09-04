af17d4e0e737b03ea2f3fa5d94d683275c16df0a
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-04 03:04:36 +0000

    Create utils/db.js

---
+++ utils/db.js
@@ -0,0 +1,32 @@
+import { MongoClient } from 'mongodb';
+import { env } from 'process';
+
+class DBClient {
+  constructor() {
+    const host = env.DB_HOST || 'localhost';
+    const port = env.DB_PORT || '27017';
+    const database = env.DB_DATABASE || 'files_manager';
+    this.client = MongoClient(`mongodb://${host}:${port}/${database}`);
+    this.client.connect();
+  }
+
+  isAlive() {
+    return this.client.isConnected();
+  }
+
+  async nbUsers() {
+    const db = this.client.db();
+    const users = db.collection('users');
+    return users.countDocuments();
+  }
+
+  async nbFiles() {
+    const db = this.client.db();
+    const files = db.collection('files');
+    return files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+
+export default dbClient;
