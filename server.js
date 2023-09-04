820c946e8cc08a6b6c3a67e2ab4c01cd90979163
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-04 03:58:47 +0000

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import {env } from 'process';
+
+const router = require('./routes/index');
+
+const app = express();
+const PORT = env.PORT || 5000;
+app.use(express.json());
+app.use(router);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+export default app;
