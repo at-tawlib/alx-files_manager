705a18640b0e98b0d590fa9117110f71ee39ef1d
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-04 03:58:40 +0000

    Create AppController and index.js route

---
+++ controllers/AppController.js
@@ -0,0 +1,19 @@
+import redisClient from '../utils/redis';
+import dbClient from '../utils/db';
+
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).json({ redis: true, db: true });
+    }
+  }
+
+  static async getStats(req, res) {
+    const usersCount = await dbClient.nbUsers();
+    const filesCount = await dbClient.nbFiles();
+    res.status(200).json({ users: usersCount, files: filesCount });
+    res.end();
+  }
+}
+
+export default AppController;
