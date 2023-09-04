71c33019a8cb00ca694662a976aa959355a79107
Abdul-Fatahu Hardi <a.fatahu95@gmail.com>
2023-09-04 03:56:18 +0000

    Change localhost

--- utils/db.js
+++ utils/db.js
@@ -3,11 +3,11 @@ import { env } from 'process';
 
 class DBClient {
   constructor() {
-    const host = env.DB_HOST || 'localhost';
+    const host = env.DB_HOST || '127.0.0.1';
     const port = env.DB_PORT || '27017';
     const database = env.DB_DATABASE || 'files_manager';
     this.client = MongoClient(`mongodb://${host}:${port}/${database}`);
-    this.client.connect();
+    this.client.connect({ useNewUrlParser: true, useUnifiedTopology: true });
   }
 
   isAlive() {
